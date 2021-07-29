0e5384241bb3dab9fc1715f96db4c68f20788bb5
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-07-28 22:03:02 -0400

    Renamed DLL for CKAN compatibility Added AssemblyFileVersion Updated version file for 1.12

--- KSP_DataDump/PositionDump.cs
+++ KSP_DataDump/PositionDump.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+//using UnityEngine.Experimental.PlayerLoop;
 using static KSP_DataDump.DataDump;
 #if false
 namespace KSP_DataDump
